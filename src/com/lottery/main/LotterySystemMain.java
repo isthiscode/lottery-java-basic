5b0183482da475926093b31499e7d8b70b5026fd
isthiscode <mithu.gr8@gmail.com>
2017-04-08 06:51:42 -0700

    Minor: added a description for the main class

--- src/com/lottery/main/LotterySystemMain.java
+++ src/com/lottery/main/LotterySystemMain.java
@@ -1,5 +1,11 @@
 package com.lottery.main;
 
+/***
+ * Main class to start the entire system.
+ *
+ * @author meera
+ *
+ */
 public class LotterySystemMain {
 
     public static void main(String[] args) {
